c6b78f834b1b6c004980c57e0392f86a14c36fa0
cesar linux <cesarlinux@gmail.com>
2017-10-25 11:46:55 -0300

    add pack

--- testconnect.ts
+++ testconnect.ts
@@ -1,22 +1,21 @@
 import * as sql from 'mssql';
-import * as config from './config';            
-            
-            
-            var connection = {
-                user: 'sa',
-                password: '12345678',
-                server: '127.0.0.1',
-                database: 'MARA',
-                // connectionTimeout: config.connectionTimeout,
-                requestTimeout: config.requestTimeout
+import * as config from './config';
 
-            };
+var connection = {
+    user: 'sa',
+    password: 'ssecure',
+    server: '127.0.0.1',
+    database: 'SIPS',
+    // connectionTimeout: config.connectionTimeout,
+    requestTimeout: config.requestTimeout
+};
 
-            sql.connect(connection).then(function() {
-            console.log('Conexion')
-            })
-             .catch(function(err) {
-                        // ... query error checks
-                        console.log("Error de conexión al server", err);
-                        
-                    });
+sql.connect(connection).then(function () {
+    console.log('Conexion');
+    sql.close();
+})
+    .catch(function (err) {
+        // ... query error checks
+        console.log("Error de conexión al server", err);
+
+    });
