bc297b785cf095dfbde0c05c8ee2beffd1400f3f
cesar linux <cesarlinux@gmail.com>
2018-03-01 15:40:09 -0300

    version final que funciona abriendo y cerrando las cx y limpieza de codigo

--- importarPuco.ts
+++ importarPuco.ts
@@ -1,6 +1,3 @@
-import { servicioMongo } from './servicioMongo';
-import { servicioSips } from './servicioSips'
-
 import * as sql from 'mssql';
 import * as mongodb from 'mongodb';
 import * as config from './config';
@@ -8,9 +5,6 @@ import { debug } from 'util';
 import * as assert from 'assert';
 import { resolve } from 'path';
 import { EventEmitter } from 'events';
-
-//var srvSips = new servicioSips();
-//var srvMongo = new servicioMongo();
 
 const connection = {
     user: config.user,
@@ -20,7 +14,6 @@ const connection = {
 };
 const url = config.urlMigracion;
 
-//var offset = 0;
 var limit = 999;
 var max = 5000000;
 
@@ -31,57 +24,29 @@ getPuco();
 
 async function insertmongo(datos) {
     try {
-        let conn = await mongodb.MongoClient.connect(url)
-        let db = await conn.db('andes')
-        // When the mongodb server goes down, the driver emits a 'close' event
-        db.on('close', () => { console.log('-> lost mongodb connection'); });
-        // The driver tries to automatically reconnect by default, so when the
-        // server starts the driver will reconnect and emit a 'reconnect' event.
-        db.on('reconnect', () => { console.log('-> mongodb reconnected'); });
-
-        let result = await db.collection('puco').insertMany(datos.recordset, { ordered: false })
-        // console.dir(result)
+        let conn = await mongodb.MongoClient.connect(url);
+        let db = await conn.db('andes');
+        let result = await db.collection('puco').insertMany(datos, { ordered: false })
+        conn.close();
     } catch (err) {
         console.log("error insertMongo----", err)
     }
 }
 
 async function getPuco() {
-    console.log("Entra a gePuco")
-    let pool = await sql.connect(connection);
-    sql.on('error', err => {
-        // ... error handler
-        console.log(err);
-    });
     for (var i = 0; i <= max; i += limit) {
         try {
+            let pool = await sql.connect(connection);
             let result1 = await pool.request()
                 .input('offset', sql.Int, i)
                 .input('limit', sql.Int, limit)
                 .query(config.consultaPuco)
-            const inserciones = await insertmongo(result1);
+            const inserciones = await insertmongo(result1.recordset);
+            sql.close();
             console.dir(i);
         } catch (err) {
             // ... error checks
             console.log("ERROR getPuco---", err);
         }
     }
-    pool.close();
 }
-
-/*
-
-def source =  https://www.toptal.com/javascript/asynchronous-javascript-async-await-tutorial
-
- const verifyUser = async function(username, password){
-   try {
-       const userInfo = await dataBase.verifyUser(username, password); 
-       const rolesInfo = await dataBase.getRoles(userInfo);
-       const logStatus = await dataBase.logAccess(userInfo);
-       return userInfo;
-   }catch (e){
-       //handle errors as needed
-   }
-};
-
-*/
