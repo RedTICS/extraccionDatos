33402a603cf4e0831915ed91b226b6bd65ebdc2b
cesar linux <cesarlinux@gmail.com>
2018-09-27 12:03:00 -0300

    agregacion labo

--- agregationLaboratorioPractica.js
+++ agregationLaboratorioPractica.js
@@ -1,18 +1,30 @@
-db.practicaTemp.aggregate(
-    [
-        // Stage 1
-        {
-            $project: {
-                codigo: 1,
-                nombre: 1,
-                codigoNomenclador: 1,
-                descripcion: 1,
-                concepto: { conceptId: "$conceptoSnomed" }
-            }
-        },
-        // Stage 6
-        {
-            $out: "practicaJR"
-        },
-    ]
+db.practicaTemp.aggregate(
+    [
+        // Stage 1
+        {
+            $project: {                
+                codigo: 1,
+                nombre: 1,
+                codigoNomenclador: 1,
+                descripcion: 1,
+                categoria : 1, 									
+                ordenImpresion : 1, 
+                
+                concepto: { conceptId:"$conceptoSnomed"},
+                sistema: null,
+                tipoLaboratorio: {
+                    nombre: "$tipoLaboratorio_nombre",
+                    nomencladorProvincial: 0
+                    },
+                area:{
+                    nombre: "$area_nombre",
+                    concepto: "$area_conceptoSnomed",
+                    }
+            }
+        },
+        // Stage 6
+        {
+            $out: "practicaJR"
+        },
+    ]
 );
