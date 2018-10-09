fe45c5a2194e54a3fa28846ad8403de433fcf33b
Pablo Lammel <lammelpablo@gmail.com>
2018-10-09 08:28:38 -0300

    generacion de practicas compuestas

--- importarLaboratorioPracticas.ts
+++ importarLaboratorioPracticas.ts
@@ -14,182 +14,213 @@ var myConnection = {
 };
 const url = config.urlMigracion;
 
-var limit = 999;
-var max = 20000;
+function obtenerPracticasSimples() {
+    return obtenerPracticas(consultas.consultaLaboratorioPracticasSimples);
+}   
+async function obtenerPracticasCompuestas() {
+    console.log('obtenerPracticasCompuestas');
+    return obtenerPracticas(consultas.consultaLaboratorioPracticasCompuestas);
+}
 
-
-function obtenerPracticas() {
+async function obtenerPracticas(query) {
     return new Promise((resolve, reject) => {
-        sql.connect(myConnection).then(function () {
-            new sql.Request()
-              .query(consultas.consultaLaboratorioPracticas).then(function (recordset) {
-                    resolve(recordset.recordset);        
-                }).catch(function (err) {
-                    // ... query error checks
-                    console.log("Error de conexión");
-                    reject(err);
+        new sql.Request()
+            .query(query)
+            .then((recordset) => { 
+                resolve(recordset.recordset); 
+            })
+            .catch(function (err) {
+                reject(err);
             });  
-        });  
-        
     });    
 }
 
 run();
 
 async function run() {
-    // let practicas = await obtenerPracticas();
-    // insertmongo(practicas);
-    aggregate();
-}
+    let conn = await mongodb.MongoClient.connect(url);
+    let db = await conn.db('andes');
+    await db.collection('practica').remove();
+    await db.collection('practicaTemp').remove();
+    conn.close();
 
-async function getLaboratorioPracticas(desde, hasta) {
-    return new Promise((resolve, reject) => {
-        sql.connect(myConnection).then(function () {
-            new sql.Request().query(consultas.consultaLaboratorioPracticas, [desde, hasta], function (error, results, fields) {
-                if (error) reject(error);
-                resolve(results);
-            });
-        })
-    });
+    await sql.connect(myConnection);
+    await insertmongo(await obtenerPracticasSimples());
+    await aggregate(false);
+    await insertmongo(await obtenerPracticasCompuestas());
+    await aggregate(true);
+    sql.close();
+    
+    console.log("end of program")
 }
 
 async function insertmongo(datos) {
     try {
         let conn = await mongodb.MongoClient.connect(url);
         let db = await conn.db('andes');
-        let result = await db.collection('practicaTempAll').insertMany(datos, { ordered: false })
+        let result = await db.collection('practicaTemp').insertMany(datos, { ordered: false })
         conn.close();
-        console.log("inserted", datos.length)
     } catch (err) {
         console.log("error insertMongo----", err, "datos: ", datos);
     }
 }
 
-async function aggregate(){
-    console.log('aggregate')
+async function aggregate(compuestas){
+    console.log('aggregate', compuestas);
     let conn = await mongodb.MongoClient.connect(url);
     let db = await conn.db('andes');
-    // await db.collection('practicaTemp').aggregate([
-    //     {
-    //        $project: {
-    //           codigo: 1,
-    //           nombre: 1,
-    //           codigoNomenclador: 1,
-    //           descripcion: 1,
-    //           categoria: 1,
-    //           ordenImpresion: 1,
-    //           etiquetaAdicional: 1,
-    //           concepto: { conceptId: "$conceptoSnomed" },
-    //           sistema: null,
-    //           tipoLaboratorio: {
-    //               nombre: "$tipoLaboratorio_nombre"
-    //           },
-    //               area: {
-    //                   nombre: "$area_nombre",
-    //                   concepto: {
-    //                       conceptId: "$area_conceptoSnomed"
-    //                   }
-    //               },
-    //               unidadMedida: {
-    //                   nombre: "$unidadMedida_nombre",
-    //                   concepto: {
-    //                       conceptId: "$unidadMedida_concepto_conceptId"
-    //                   }
-    //               },
-    //               requeridos: [],
-    //               resultado: {
-    //                    formato: {
-    //                        opciones: []
-    //                    }
-    //               },
-    //               metodo: {
-    //                   nombre: "$metodo_nombre",
-    //                   valoresReferencia: {
-    //                       sexo: "$metodo_valoresReferencia_sexo",
-    //                       todasEdades: "$metodo_valoresReferencia_todasEdades",
-    //                       edadDesde: "$metodo_valoresReferencia_edadDesde",
-    //                       edadHasta: "$metodo_valoresReferencia_edadHasta",
-    //                       unidadEdad: "$metodo_valoresReferencia_unidadEdad",
-    //                       tipoValor: "$metodo_valoresReferencia_tipoValor",
-    //                       valorMinimo: "$metodo_valoresReferencia_valorMinimo",
-    //                       valorMaximo: "$metodo_valoresReferencia_valorMaximo",
-    //                       observacion: "$metodo_valoresReferencia_observacion",
-    //                       activo: "$metodo_valoresReferencia_activo",
-    //                       reactivo: ["$metodo_valoresReferencia_reactivo"]
-    //                   },
-    
-    //               },
-    //               valoresCriticos: {
-    //                   minimo: "$valoresCriticos_minimo",
-    //                   maximo: "$valoresCriticos_maximo"
-    //               },
-    //               recomendaciones: [],
-    
-    //               factorProduccion: null,
-    //               opciones: {$cond: [
-    //                   { $eq: ["$resultado_formato_opciones", null] },
-    //                     [],
-    //                     "$resultado_formato_opciones"
-    //               ]}
-    //           }
-    //        },
-    //        {
-    //            $group: {
-    //                _id: {codigo: '$codigo'},
-    //                codigo: { $first: '$codigo'},
-    //                nombre: { $first: '$nombre'},
-    //                codigoNomenclador: { $first: '$codigoNomenclador'},
-    //                descripcion: { $first: '$descripcion'},
-    //                categoria: { $first: '$categoria'}, 									
-    //                ordenImpresion: { $first: '$ordenImpresion'},
-    //                concepto: { $first: "$conceptoSnomed" },
-    //                sistema: { $first: '$sistema' },
-    //                tipoLaboratorio: { $first: "$tipoLaboratorio"},
-    //                area: { $first: '$area' },
-    //                unidadMedida: { $first: '$unidadMedida'},
-    //                requeridos: { $first: '$requeridos'},
-    //                resultado: { $first: '$resultado'},
-    //                metodo: { $first: '$metodo'},
-    //                valoresCriticos: { $first: '$valoresCriticos'},
-    //                recomendaciones: { $first: '$recomendaciones'},
-    //                factorProduccion: { $first: '$factorProduccion'},
-    //                opciones: {$push: '$opciones'}
-    //            }
-    //        },
-    //        {
-    //            $addFields: {
-    //                "resultado.formato.opciones": '$opciones'
-    //            }
-    //        },
-    //        { $out: 'practicaRulo'},
-    //        { $out: "practicaJR" }
-    // ]);
-    // console.log('agregacioens terminadas..........')        
-    // db.collection('practicaRulo').update({}, {$unset: {opciones:1}}, false, true)
-    // console.log('practicas zlxfvnzsdfshrfczhlqy')
-    
-    await db.collection('practica').find({}).toArray(function(err, practicas) {
-        console.log('practicas', practicas.length)
-        let promises = [];
-        practicas.map( (practica) => {
-            promises.push(new Promise((resolve, reject) => {
-                setSCTConcept(practica._id, practica.concepto.conceptId,'concepto', resolve);
-            }));
-            promises.push(new Promise((resolve, reject) => {
-                setSCTConcept(practica._id, practica.unidadMedida.concepto.conceptId, 'unidadMedida', resolve);
-            }));
+    const project: any = {
+        codigo: 1,
+        nombre: 1,
+        codigoNomenclador: 1,
+        descripcion: 1,
+        categoria: 1,
+        ordenImpresion: 1,
+        etiquetaAdicional: 1,
+        concepto: { conceptId: "$conceptoSnomed" },
+        sistema: null,
+        tipo: compuestas ? 'Compuesta' : 'Simple',
+        tipoLaboratorio: {
+            nombre: "$tipoLaboratorio_nombre"
+        },
+        area: {
+            nombre: "$area_nombre",
+            concepto: {
+                conceptId: "$area_conceptoSnomed"
+            }
+        },
+        unidadMedida: {
+            nombre: "$unidadMedida_nombre",
+            concepto: {
+                conceptId: "$unidadMedida_concepto_conceptId"
+            }
+        },
+        resultado: {
+            formato: {
+                tipo: "$resultado_formato_tipo",
+                decimales: "$resultado_formato_decimales",
+                multiplicador: "$resultado_formato_multiplicador",
+                notacionCientifica: {
+                    exponentePredeterminado: null
+                },
+                limiteDeteccion: {  /* para poner "menor que 10", valor numerico (numero entre 10 y 50), o "mayor que 50" */
+                    minimo: null,
+                    maximo: null
+                },
+                opciones: []
+            },
+            valorDefault: "$resultado_valorDefault"
+        },
+        presentaciones: {
+            denominacion: "$metodo_valoresReferencia_reactivo",
+            fabricante: {
+                nombre: "",
+                numero: ""
+            },
+            metodo: "$metodo_nombre",
+            valoresReferencia: [{
+                sexo: "$metodo_valoresReferencia_sexo",
+                todasEdades: "$metodo_valoresReferencia_todasEdades",
+                edadDesde: "$metodo_valoresReferencia_edadDesde",
+                edadHasta: "$metodo_valoresReferencia_edadHasta",
+                unidadEdad: "$metodo_valoresReferencia_unidadEdad",
+                tipoValor: "$metodo_valoresReferencia_tipoValor",
+                valorMinimo: "$metodo_valoresReferencia_valorMinimo",
+                valorMaximo: "$metodo_valoresReferencia_valorMaximo",
+                observacion: "$metodo_valoresReferencia_observacion",
+                activo: "$metodo_valoresReferencia_activo"
+            }],
+        },
+        valoresCriticos: {
+            minimo: "$valoresCriticos_minimo",
+            maximo: "$valoresCriticos_maximo"
+        },
+        recomendaciones: [{
+            nombre: "$recomendaciones_nombre",
+            descripcion: "$recomendaciones_descripcion",
+        }],
+        factorProduccion: null,
+        opciones: {
+            $cond: [
+                { $eq: ["$resultado_formato_opciones", null] },
+                [],
+                "$resultado_formato_opciones"
+            ]
+        },
+        requeridos: {
+            codigo: '$subitem_codigo',
+            codigoNomenclador: '$subitem_codigoNomenclador',
+            nombre: '$subitem_nombre'
+        }
+    };
+
+    const group = {
+        _id: { codigo: '$codigo' },
+        codigo: { $first: '$codigo' },
+        nombre: { $first: '$nombre' },
+        codigoNomenclador: { $first: '$codigoNomenclador' },
+        descripcion: { $first: '$descripcion' },
+        categoria: { $first: '$categoria' },
+        ordenImpresion: { $first: '$ordenImpresion' },
+        concepto: { $first: "$concepto" },
+        sistema: { $first: '$sistema' },
+        tipoLaboratorio: { $first: "$tipoLaboratorio" },
+        area: { $first: '$area' },
+        unidadMedida: { $first: '$unidadMedida' },
+        requeridos: { $push: '$requeridos' },
+        resultado: { $first: '$resultado' },
+        presentaciones: { $push: '$presentaciones' },
+        valoresCriticos: { $first: '$valoresCriticos' },
+        recomendaciones: { $first: '$recomendaciones' },
+        factorProduccion: { $first: '$factorProduccion' },
+        opciones: { $push: '$opciones' }
+    };
+
+    let practicasTemp = await db.collection('practicaTemp').aggregate([
+        { $project: project },
+        { $group: group },
+        {
+            $addFields: {
+                "resultado.formato.opciones": '$opciones'
+            }
+        }
+    ]).toArray();
+
+    let practicaCollection = db.collection('practica');
+    await practicaCollection.insertMany(practicasTemp, { ordered: false })
+    await practicaCollection.update({}, { $unset: { opciones: 1 } }, false)
+    await practicaCollection.find({}).toArray(function(err, practicas) {
+        // let promises = [];
+        practicas.map( async (practica) => {
+            if (practica.categoria === 'compuesta') {
+                let codigos = [];
+                    practica.requeridos.map( (requerido) => {
+                    codigos.push(requerido.codigo)
+                });
+
+                let requeridos = await db.collection('practica').aggregate([
+                    {
+                    $match: {
+                        'codigo' : {$in: codigos }
+                    }
+                    },
+                    { $project: {_id:'$_id'} }
+                ]).toArray();
+                await practicaCollection.findAndModify({ "_id": practica._id }, [['_id','asc']], {$set: { "requeridos": requeridos } });
+            }
+            setSCTConcept(practica._id, practica.concepto.conceptId,'concepto');
+            setSCTConcept(practica._id, practica.unidadMedida.concepto.conceptId, 'unidadMedida');
         });    
     });
 
-    let setSCTConcept = (objectId, conceptId, field, resolve) => {
-        
+    await db.collection('practicaTemp').remove();
+    let setSCTConcept = (objectId, conceptId, field) => {        
         let snomedURLexpression = 'http://localhost:3002/api/core/term/snomed/expression?expression=';
-                    
+
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", snomedURLexpression + conceptId); // third parameter false for synchronous request
         xmlHttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                console.log('this.responseText',this.responseText)
                 let conceptoSnomed = JSON.parse(this.responseText).length > 0 ? JSON.parse(this.responseText)[0] : null ;
 
                 let setParam;
@@ -202,13 +233,11 @@ async function aggregate(){
                     setParam =  {$set: { "unidadMedida.concepto": conceptoSnomed }};
                     break;
                 }
-                
-                db.collection('practica').findAndModify({ "_id": objectId }, [['_id','asc']], setParam );
-                resolve(this.responseText);
+
+                practicaCollection.findAndModify({ "_id": objectId }, [['_id','asc']], setParam );
             }
         }
         xmlHttp.send();
     }
+    console.log('END AGREGGATE')
 }
-
-    
